09614d19930f4e3ac5bb216580c5da36c113a16f
EmekaOmesiete <omesietemicheal@gmail.com>
2025-10-14 10:11:51 +0100

    Worked on Login verification and did my routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import LoginForm from "./components/auth/LoginForm";
+
 
 const App = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </HashRouter>
+    
   );
 };
 
---
+++ src/components/auth/LoginForm.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { LockOutlined, MailOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input, Flex } from 'antd';
+import { useNavigate, Link } from 'react-router-dom';
+const LoginForm = () => {
+    const onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
+  const navigate = useNavigate();
+  return (
+    <Form
+      name="login"
+      initialValues={{ remember: true }}
+      style={{ maxWidth: 360 }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="email"
+        rules={[{ required: true, message: 'Please input your Email!' }]}
+      >
+        <Input prefix={<MailOutlined />} placeholder="Email" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
+      </Form.Item>
+      <Form.Item>
+        <Flex justify="space-between" align="center">
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+          <a onClick={() => navigate("/ForgotPassword")}>Forgot password</a>
+        </Flex>
+      </Form.Item>
+
+      <Form.Item>
+        <Button block type="primary" htmlType="submit">
+          Log in
+        </Button>
+        or <Link to={"/SignUp"}>Sign up!</Link>
+      </Form.Item>
+    </Form>
+  )
+}
+
+export default LoginForm
